c905d8035753384d82a9657a2ccf291e9c75f8f0
Anastasia Samilyk <asamilyk@yandex.ru>
2024-03-24 17:24:40 +0300

    update Inventory manager

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>(); 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
 }
