52af1213787cc7eb2716b25cbfef8487cbfee94f
Anastasia Samilyk <asamilyk@yandex.ru>
2024-03-25 09:46:05 +0300

    add necessary files

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -11,6 +11,7 @@ public class InventoryManager : MonoBehaviour
     public Transform ItemContent;
     public GameObject InventoryItem;
     public Toggle EnableRemove;
+    public InventoryItemController[] InventoryItems;
     private void Awake()
     {
         Instance = this;
@@ -38,10 +39,17 @@ public class InventoryManager : MonoBehaviour
             GameObject obj = Instantiate(InventoryItem, ItemContent);
             var itemName = obj.transform.Find("ItemName").GetComponent<TMP_Text>();
             var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
-
+            var removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
+            
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
+
+            if (EnableRemove.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
         }
+        SetInventoryItems();
     }
 
     public void EnableItemsRemove()
@@ -59,6 +67,15 @@ public class InventoryManager : MonoBehaviour
             {
                 item.Find("RemoveButton").gameObject.SetActive(false);
             }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+        for (int i = 0; i < Items.Count; i++)
+        {
+            InventoryItems[i].AddItem(Items[i]);
         }
     }
 }
